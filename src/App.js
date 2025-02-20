88814fa2bb482cb9a7a13d861f69d8364f65812d
Arthur <fonichkin.arthur@gmail.com>
2025-02-20 08:55:07 +0300

    update github actions

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
 
     const [version, setVersion] = React.useState('')
     const baseUrl = process.env.REACT_APP_BASE_URL;
-    const frontVersion = "2.1.0";
+    const frontVersion = "2.1.1";
 
     React.useEffect(() => {
         fetch(baseUrl +'/app/version')
