b9412b83edbb27292d6e724458c531d89dd0d175
Arthur <fonichkin.arthur@gmail.com>
2025-02-21 09:43:43 +0300

    v1.0.0

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
 
     const [version, setVersion] = React.useState('')
     const baseUrl = process.env.REACT_APP_BASE_URL;
-    const frontVersion = "4.0.1";
+    const frontVersion = "1.0.0";
 
     React.useEffect(() => {
         fetch(baseUrl +'/app/version')
